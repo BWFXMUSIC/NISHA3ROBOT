81f896d7dc809a54c761fc369cd96dec5a7647aa
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-03-25 21:25:20 +0530

    Update start.py

--- Romeo/utils/inline/start.py
+++ Romeo/utils/inline/start.py
@@ -54,6 +54,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
                 InlineKeyboardButton(text="❤️‍🔥𝐎𝚆𝙽𝙴𝚁❤️‍🔥", user_id=config.OWNER_ID),
+            ),
                 InlineKeyboardButton(text="💝𝐉𝙰𝙰𝙽💝", user_id=5493923823),
             )
         ]
