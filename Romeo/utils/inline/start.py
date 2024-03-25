4e1bfd18d1544821cdf44a7dbf61315e3d4829ee
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-03-25 21:00:46 +0530

    Update start.py

--- Romeo/utils/inline/start.py
+++ Romeo/utils/inline/start.py
@@ -56,21 +56,21 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     if GITHUB_REPO and OWNER:
         buttons.append(
             [
-                InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                InlineKeyboardButton(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
+                InlineKeyboardButton(text=_["S_B_7"], url=config.OWNER_ID
+                InlineKeyboardButton(text=_["S_B_6"], url=config.UPSTREAM_REPO
             ]
         )
     else:
         if GITHUB_REPO:
             buttons.append(
                 [
-                    InlineKeyboardButton(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
+                    InlineKeyboardButton(text=_["S_B_6"], url=config.UPSTREAM_REPO
                 ]
             )
         if OWNER:
             buttons.append(
                 [
-                    InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
+                    InlineKeyboardButton(text=_["S_B_7"], url=config.OWNER_ID
                 ]
             )
     buttons.append([InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG")])
