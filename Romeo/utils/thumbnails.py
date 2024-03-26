7b482c7d76772ca51b1da8ff3929188226239af0
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-03-26 14:48:07 +0530

    Update thumbnails.py

--- Romeo/utils/thumbnails.py
+++ Romeo/utils/thumbnails.py
@@ -122,8 +122,8 @@ async def gen_thumb(videoid, user_id):
             font4 = ImageFont.truetype('helper/rj/font2.ttf', 35)
 
             image4 = ImageDraw.Draw(image2)
-            image4.text((10, 10), "ASHISH MUSIC", fill="white", font = font1, align ="left") 
-            image4.text((670, 150), "NISHA PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
+            image4.text((10, 10), "MUSIC", fill="white", font = font1, align ="left") 
+            image4.text((670, 150), "PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
 
             # title
             title1 = truncate(title)
