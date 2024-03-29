4130ae14b2f84e41d7fc4e1e2bdb74967cbad68d
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-03-30 03:01:05 +0530

    Update play.py

--- Romeo/utils/inline/play.py
+++ Romeo/utils/inline/play.py
@@ -117,32 +117,34 @@ def stream_markup(_, videoid, chat_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="▷",
+                text="",
                 callback_data=f"ADMIN Pause|{chat_id}",
             ),
             InlineKeyboardButton(
-                text="II", callback_data=f"ADMIN Resume|{chat_id}"
+                text="", callback_data=f"ADMIN Resume|{chat_id}"
             ),
             InlineKeyboardButton(
-                text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"
+                text="", callback_data=f"ADMIN Skip|{chat_id}"
             ),
             InlineKeyboardButton(
-                text="▢", callback_data=f"ADMIN Stop|{chat_id}"
+                text="", callback_data=f"ADMIN Stop|{chat_id}"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="gh",
+                text=_["PL_B_2"],
                 callback_data=f"add_playlist {videoid}",
             ),
             InlineKeyboardButton(
-                text="fi", url=f"{config.SUPPORT_GROUP}"
-            )
+                text="✨ 𝐎ωиєя ⏎", url=f"https://t.me/L2R_KING0"
+            ),
         ],
         [
             InlineKeyboardButton(
-                text="❌ 𝐂𝐥𝐨𝐬𝐞 ❌", callback_data="close"
-            )
+                text=_["PL_B_3"],
+                callback_data=f"PanelMarkup None|{chat_id}",
+            ),
+            InlineKeyboardButton(text="⛩️ Gʀᴏᴜᴘꨄ︎ ⛩️", url=f"https://t.me/BWF_MUSIC1"),
         ],
     ]
     return buttons
@@ -167,11 +169,11 @@ def telegram_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="to",
+                text="",
                 callback_data=f"add_playlist {videoid}",
             ),
             InlineKeyboardButton(
-                text="AshishMusic2", url=f"{config.SUPPORT_GROUP}"
+                text="", url=f"{config.SUPPORT_GROUP}"
             )
         ],
         [
@@ -328,11 +330,11 @@ def queue_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="xxxx",
+                text="",
                 callback_data=f"add_playlist {videoid}",
             ),
             InlineKeyboardButton(
-                text="xxxx", url=f"{config.SUPPORT_GROUP}"
+                text="", url=f"{config.SUPPORT_GROUP}"
             )
         ],
         [
@@ -363,11 +365,11 @@ def telegram_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="zzzzz",
+                text="",
                 callback_data=f"add_playlist {videoid}",
             ),
             InlineKeyboardButton(
-                text="zzzzzz", url=f"{config.SUPPORT_GROUP}"
+                text="", url=f"{config.SUPPORT_GROUP}"
             )
         ],
         [
