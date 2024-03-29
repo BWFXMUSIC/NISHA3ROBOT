99c1ca26395aff03bf09bc8e04244eb4446d1d50
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-03-29 23:29:17 +0530

    Update Telegraph.py

--- Romeo/plugins/admins/Telegraph.py
+++ Romeo/plugins/admins/Telegraph.py
@@ -1,56 +1,16 @@
-import os, asyncio
-from typing import Optional
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
 from telegraph import upload_file
+from pyrogram import filters
 from Romeo import app
 
 
-#---------------FUNCTION---------------#
+@app.on_message(filters.command('tgm'))
+def ul(_, message):
+    reply = message.reply_to_message
+    if reply.media:
+        i = message.reply("🌹ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ...ʙʏ ᴀᴀʀᴏʜɪ ᴍᴜsɪᴄ🌹")
+        path = reply.download()
+        fk = upload_file(path)
+        for x in fk:
+            url = "https://telegra.ph" + x
 
-def get_file_id(msg: Message) -> Optional[Message]:
-    if not msg.media:
-        return None
-
-    for message_type in ("photo", "animation", "audio", "document", "video", "video_note", "voice", "sticker"):
-        obj = getattr(msg, message_type)
-        if obj:
-            setattr(obj, "message_type", message_type)
-            return obj
-
-#---------------FUNCTION---------------#
-
-
-@app.on_message(filters.command("tgm"))
-async def telegraph_upload(bot, update):
-    replied = update.reply_to_message
-    if not replied:
-        return await update.reply_text("Rᴇᴘʟʏ Tᴏ A Pʜᴏᴛᴏ Oʀ Vɪᴅᴇᴏ Uɴᴅᴇʀ 5ᴍʙ")
-    file_info = get_file_id(replied)
-    if not file_info:
-        return await update.reply_text("Not Supported!")
-    text = await update.reply_text(text="<code>Downloading To My Server ...</code>", disable_web_page_preview=True)   
-    media = await update.reply_to_message.download()   
-    await text.edit_text(text="<code>Downloading Completed. Now I am Uploading to telegra.ph Link ...</code>", disable_web_page_preview=True)                                            
-    try:
-        response = upload_file(media)
-    except Exception as error:
-        print(error)
-        await text.edit_text(text=f"Error :- {error}", disable_web_page_preview=True)       
-        return    
-    try:
-        os.remove(media)
-    except Exception as error:
-        print(error)
-        return    
-    await text.edit_text(
-        text=f"<b>ʜᴇʀᴇ ɪs ʏᴏᴜʀ ɢᴇɴᴇʀᴀᴛᴇᴅ ᴛᴇ.ʟᴇɢʀᴀ.ᴘʜ ʟɪɴᴋ 💞 :-</b>\n\n<code>https://te.legra.ph{response[0]}</code>",
-        disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup( [[
-            InlineKeyboardButton(text="ᴏᴘᴇɴ ʟɪɴᴋ", url=f"https://te.legra.ph{response[0]}"),
-            InlineKeyboardButton(text="sʜᴀʀᴇ ʟɪɴᴋ", url=f"https://telegram.me/share/url?url=https://te.legra.ph{response[0]}")
-            ],[
-            InlineKeyboardButton(text="✗ ᴄʟᴏsᴇ ✗", callback_data="close")
-            ]])
-        )
-    
+        i.edit(f'🇾ᴏᴜʀ🇹ᴇʟᴇɢʀᴀᴘʜ 👉 {url}')
